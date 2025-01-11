f97e8096c1a0c1e92708112ecf50706105a0d1e1
Stazzy <freef@tutamail.com>
2025-01-11 07:00:42 -0500

    Add files via upload

--- Random.js
+++ Random.js
@@ -1,1 +1,1 @@
-var sites = ['text-1.html']; function randomSite() {var i = parseInt(Math.random() * sites.length); location.href = sites[i];}
+var sites = ['text-1.html', 'text-2.html']; function randomSite() {var i = parseInt(Math.random() * sites.length); location.href = sites[i];}
